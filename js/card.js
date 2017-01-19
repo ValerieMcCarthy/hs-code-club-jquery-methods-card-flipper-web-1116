4af38203f75106cd752aecf4eb4e2a4ebec5daf8
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2017-01-19 06:43:01 -0500

    Pretty kitty cards are flipping

--- js/card.js
+++ js/card.js
@@ -1,11 +1,25 @@
 $( function(){
 
   $( "#card-one").click(function(){
-    //Use toggleClass method here
+  		$(this).toggleClass("flipped")
   });
 
+  // if ( $( this ).parent().is( ".flipped" ) ) {
+  //   return "front panel";
+  // } else {
+  //   return "back panel";
+  // }
 
 	// code click event for card-two here
+	// 
+	$( "#card-two").click(function(){
+  		$(this).toggleClass("flipped")
+  });
 
   // code click event for card-three here
- })
+
+$( "#card-three").click(function(){
+  		$(this).toggleClass("flipped")
+  });
+
+})
